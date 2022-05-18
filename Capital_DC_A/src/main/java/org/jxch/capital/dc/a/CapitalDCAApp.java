99b200b461f51da1ccd81a6f4dd69fe4d505c3d8
jxch <15553598935@163.com>
2022-05-18 10:47:05 +0800

    capital dc a init

---
+++ Capital_DC_A/src/main/java/org/jxch/capital/dc/a/CapitalDCAApp.java
@@ -0,0 +1,11 @@
+package org.jxch.capital.dc.a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CapitalDCAApp {
+    public static void main(String[] args) {
+        SpringApplication.run(CapitalDCAApp.class, args);
+    }
+}
---
+++ Capital_DC_A/src/test/java/org/jxch/CapitalDCAAppTest.java
@@ -0,0 +1,20 @@
+package org.jxch;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class CapitalDCAAppTest
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
