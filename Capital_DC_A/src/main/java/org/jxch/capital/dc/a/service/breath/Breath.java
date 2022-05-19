6096931a775aa36236cca047a8ef1825220d5f95
jxch <15553598935@163.com>
2022-05-19 17:42:53 +0800

    添加ojdbc8依赖

--- Capital_DC_A/src/main/java/org/jxch/capital/dc/a/service/breath/Breath.java
+++ Capital_DC_A/src/main/java/org/jxch/capital/dc/a/service/breath/Breath.java
@@ -2,9 +2,7 @@ package org.jxch.capital.dc.a.service.breath;
 
 import org.jxch.capital.dc.a.service.breath.model.StockSectorScore;
 
-import java.util.Date;
 import java.util.List;
-import java.util.Map;
 
 public interface Breath {
     List<List<StockSectorScore>> breathByMA(int ma);
--- Capital_DC_A/src/main/java/org/jxch/capital/dc/a/service/dc/nets/NetsStockDailyDCService.java
+++ Capital_DC_A/src/main/java/org/jxch/capital/dc/a/service/dc/nets/NetsStockDailyDCService.java
@@ -1,7 +1,21 @@
 package org.jxch.capital.dc.a.service.dc.nets;
 
+import org.jxch.capital.dc.a.service.dc.model.StockKLine;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Map;
 
 @Service("netsStockDailyDC")
 public class NetsStockDailyDCService implements NetsStockDailyDC {
+    @Override
+    public List<StockKLine> dailyKLineSortedByDate(String stock) {
+
+        return null;
+    }
+
+    @Override
+    public Map<String, List<StockKLine>> dailyKLineSortedByDate(List<String> stocks) {
+        return null;
+    }
 }
