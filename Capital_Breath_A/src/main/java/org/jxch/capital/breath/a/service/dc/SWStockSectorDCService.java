788dd87e25f9811890b48840f59bc55169cf9c27
jxch <15553598935@163.com>
2022-05-21 00:32:10 +0800

    读取资源文件

--- Capital_Breath_A/src/main/java/org/jxch/capital/breath/a/service/dc/SWStockSectorDCService.java
+++ Capital_Breath_A/src/main/java/org/jxch/capital/breath/a/service/dc/SWStockSectorDCService.java
@@ -2,6 +2,7 @@ package org.jxch.capital.breath.a.service.dc;
 
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.IOUtils;
 import org.jetbrains.annotations.NotNull;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -10,11 +11,14 @@ import org.jsoup.select.Elements;
 import org.jxch.capital.breath.a.model.Stock;
 import org.jxch.capital.breath.a.model.StockSector;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -23,16 +27,7 @@ import java.util.stream.Collectors;
 
 @Service
 public class SWStockSectorDCService implements StockSectorDC {
-    private File file;
-
-    @PostConstruct
-    private void init() {
-        try {
-            file = new ClassPathResource("/sector/SW.htm", this.getClass().getClassLoader()).getFile();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
+    private final Resource file = new ClassPathResource("sector/SW.htm");
 
     @Override
     public List<StockSector> getStockSectors() {
@@ -41,12 +36,9 @@ public class SWStockSectorDCService implements StockSectorDC {
 
     @NotNull
     private List<StockSector> convert() {
-        if (!file.exists())
-            throw new RuntimeException("No File!");
-
         Map<String, List<Stock>> stockSectors;
-        try {
-            String html = FileUtils.readFileToString(file, "GB2312");
+        try (InputStream bis = new BufferedInputStream(file.getInputStream())) {
+            String html = IOUtils.toString(bis, "GB2312");
             Document doc = Jsoup.parse(html);
             Elements elements = doc.select("tr");
 
