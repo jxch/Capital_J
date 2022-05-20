8f5ac13c362561d9989c225aa4130abd4101f993
jxch <15553598935@163.com>
2022-05-20 23:40:22 +0800

    获取资源文件

--- Capital_Breath_A/src/main/java/org/jxch/capital/breath/a/service/dc/NetsStockDailyDCService.java
+++ Capital_Breath_A/src/main/java/org/jxch/capital/breath/a/service/dc/NetsStockDailyDCService.java
@@ -34,7 +34,7 @@ public class NetsStockDailyDCService implements StockDailyDC {
     @PostConstruct
     private void init() {
         try {
-            folderPath = Paths.get(new ClassPathResource("").getFile().getAbsolutePath() + "stock/daily");
+            folderPath = Paths.get(new ClassPathResource("").getFile().getAbsolutePath() + "/stock/daily");
             if (!Files.exists(folderPath)) {
                 Files.createDirectories(folderPath);
             }
